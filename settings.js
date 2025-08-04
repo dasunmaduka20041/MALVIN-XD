559e45dc43246a1c8e8aa97d07fdba86f7163899
dasunmaduka20041 <144513258+dasunmaduka20041@users.noreply.github.com>
2025-08-04 19:16:09 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ LINK_WHITELIST: "youtube.com,github.com",
         AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
 // make true if you want auto reply on status 
 
-        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "* 😆🥲*",
+        AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "**",
 // set the auto reply massage on status reply  
 
 
